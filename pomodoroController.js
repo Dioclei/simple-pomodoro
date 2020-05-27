45c9d951e632428727b3deedfa2dde7168134142
Jason Lee <jasonepic0801@gmail.com>
2020-05-27 23:12:20 +0800

    cycles resets properly

--- pomodoroController.js
+++ pomodoroController.js
@@ -7,6 +7,7 @@ let pomodoroController = new Vue({
     timerId: '',
     showInfoBar: true,
     cycles: 5,
+    cyclesInitial: 5,
     workMinutes: 25,
     breakMinutes: 5,
     editTitle: 'Double click to edit'
@@ -44,27 +45,35 @@ let pomodoroController = new Vue({
       if (this.pomodoroIsRunning) {
         if (window.confirm('This will stop and cancel the pomodoro timer. Are you sure?')) {
           // BUG: confirm stops ALL SCRIPTS from running. calculate time left from a start date/end date instead.
-          window.clearInterval(this.timerId)
-          this.timeLeft = this.timeInitial
-          this.timerId = ''
-          this.pomodoroIsRunning = false
+          this.endTimer()
         }
       } else {
-        this.timeInitial = this.timeLeft
-        this.timerId = window.setInterval(this.countdown, 1000)
-        this.pomodoroIsRunning = true
+        this.cyclesInitial = this.cycles;
+        this.startTimer()
       }
-      console.log('action button clicked!')
     },
     countdown() {
       this.timeLeft = this.timeLeft - 1
       if (this.timeLeft <= 0) {
-        window.clearInterval(this.timerId)
-        this.timeLeft = this.timeInitial
-        this.timerId = ''
-        this.pomodoroIsRunning = false
-        // TODO: update this.cycles and restart the timer promptly.
+        this.endTimer()
+        if (this.cycles >= 1) {
+          this.cycles = this.cycles - 1
+          this.startTimer()
+          return;
+        }
+        this.cycles = this.cyclesInitial
       }
+    },
+    startTimer() {
+      this.timeInitial = this.timeLeft
+      this.timerId = window.setInterval(this.countdown, 1000)
+      this.pomodoroIsRunning = true
+    },
+    endTimer() {
+      window.clearInterval(this.timerId)
+      this.timeLeft = this.timeInitial
+      this.timerId = ''
+      this.pomodoroIsRunning = false
     },
     addCycles() {
       this.cycles = this.cycles + 1
