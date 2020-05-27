a651ebe26014ca16a83f183493d0a21d5c947f6e
Jason Lee <jasonepic0801@gmail.com>
2020-05-27 11:22:44 +0800

    setting up infrastructure for info bar

---
+++ infobar.js
@@ -0,0 +1,20 @@
+let cycles = new Vue({
+  el: '#cycles',
+  data: {
+    cycles: '5'
+  }
+})
+
+let workMinutes = new Vue({
+  el: '#work',
+  data: {
+    workMinutes: '25',
+  },
+})
+
+let breakMinutes = new Vue({
+  el: '#break',
+  data: {
+    breakMinutes: '5'
+  }
+})
